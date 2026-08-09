95601a35fe27322f3fbc023baa72c9b950274cb9
אלעזר גרשי <emg4706@gmail.com>
2026-08-09 11:43:19 +0300

    created Shelter model

---
+++ Models/Shelter.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterApi.Models;
+
+public class Shelter
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(100)]
+    public string Street { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(20)]
+    public string BuildingNumber { get; set; } = string.Empty;
+}
