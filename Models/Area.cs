10aa2a2bcc7e81e8a8fb70589bb812772c21aeee
אלעזר גרשי <emg4706@gmail.com>
2026-08-09 11:31:34 +0300

    push for Noam

---
+++ Models/Area.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShelterApi.Models;
+
+public class Area
+{
+    public int Id { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string City { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(100)]
+    public string Neighborhood { get; set; } = string.Empty;
+
+    [Required]
+    [MaxLength(20)]
+    
+
+}
