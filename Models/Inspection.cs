79194461dbca5b4b2d0fb4cddd9be53c2b544b5f
אלעזר גרשי <emg4706@gmail.com>
2026-08-09 12:05:40 +0300

    created Inspection model

---
+++ Models/Inspection.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace ShelterApi.Models;
+
+public class Inspection
+{
+    public int Id { get; set; }
+
+    [Required]
+    public DateTime InspectionDate { get; set; }
+
+    [Range(0, 100)]
+    public int ReadinessScore { get; set; }
+
+    [Required]
+    public bool Passed { get; set; }
+
+    [Range(0, 100)]
+    public int DefectsCount { get; set; }
+
+    [MaxLength(500)]
+    public string Notes { get; set; } = string.Empty;
+}
--- Models/Shelter.cs
+++ Models/Shelter.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using ShelterApi.Enums;
 
 namespace ShelterApi.Models;
 
@@ -17,4 +18,17 @@ public class Shelter
     [Required]
     [MaxLength(20)]
     public string BuildingNumber { get; set; } = string.Empty;
+
+    [Range(1, 10000)]
+    public int Capacity { get; set; }
+
+    [Required]
+    public bool IsAccessible { get; set; }
+
+    [Required]
+    public bool IsPublic { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public ShelterTypes ShelterType { get; set; }
 }
